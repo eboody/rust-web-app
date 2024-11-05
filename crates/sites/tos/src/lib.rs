a0d0357a5f5cd32e36eec347f55118bdd08a4553
Eran Boodnero <eboodnero@gmail.com>
2024-11-05 05:34:05 -0800

    integrated vite+alpine

--- crates/sites/tos/src/lib.rs
+++ crates/sites/tos/src/lib.rs
@@ -1,5 +1,8 @@
+mod config;
+use config::web_config;
 pub use inline_css::css;
 pub use ryde::*;
+use serde_json::Value;
 
 pub fn router() -> Router {
 	Router::new().route("/", get(get_slash))
@@ -15,27 +18,57 @@ async fn get_slash() -> Html {
 	.to_string();
 
 	html! {
-		<Html>
+		<Page>
 			<div>
 				<style>{styles}</style>
 				you are here
 			</div>
-		</Html>
+		</Page>
 	}
 }
 
 #[allow(non_snake_case)]
-pub fn Html(els: Elements) -> Component {
+pub fn Page(els: Elements) -> Component {
+	let folder = &web_config().WEB_FOLDER;
+
+	let manifest: Value = serde_json::from_str(
+		&std::fs::read_to_string(format!("{}/js/.vite/manifest.json", folder))
+			.expect("manifest.json not found"),
+	)
+	.expect("Error parsing manifest.json");
+
+	let main_js = manifest["index.html"]["file"]
+		.as_str()
+		.expect("index.html not found in manifest");
+	let main_css = manifest["index.html"]["css"][0]
+		.as_str()
+		.expect("index.css not found in manifest");
+
 	html! {
 		<!DOCTYPE html>
 		<html>
 			<head>
+				<link rel="stylesheet" href={format!("/js/{}", main_css)}>
+				<script type="module" src={format!("/js/{}", main_js)}></script>
 				<script src="https://unpkg.com/htmx.org@2.0.3"></script>
 				<script src="https://cdn.jsdelivr.net/gh/gnat/css-scope-inline@main/script.js"></script>
 				<script src="https://cdn.jsdelivr.net/gh/gnat/surreal@main/surreal.js"></script>
 				<title>The Objective Standard</title>
 			</head>
 			<body>
+				<div id="app" x-data="app">
+					<a href="https://vitejs.dev" target="_blank">
+						<img x-bind:src="viteLogo" class="logo" alt="Vite logo" />
+					</a>
+					<a href="https://alpinejs.dev/" target="_blank">
+						<img x-bind:src="alpineLogo" class="logo alpine" alt="Alpine logo" />
+					</a>
+					<h1>Hello Vite + Alpine!</h1>
+					<div class="card">
+						<button id="counter" type="button" x-on:click="count++" x-text="`count is ${count}`"></button>
+					</div>
+					<p class="read-the-docs">Click on the Vite and Alpine logos to learn more</p>
+				</div>
 				{els}
 			</body>
 		</html>
---
+++ vite-alpine/main.js
@@ -0,0 +1,13 @@
+import './style.css';
+import alpineLogo from './alpine_long.svg';
+import viteLogo from '/vite.svg';
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+Alpine.data('app', () => ({
+  viteLogo,
+  alpineLogo,
+  count: 0,
+})
+)
+Alpine.start();
