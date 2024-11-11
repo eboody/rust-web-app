4b7191a1eeefb7ccc429dcc097f80f4a4a07a7b3
Eran Boodnero <eboodnero@gmail.com>
2024-11-11 15:01:52 -0800

    fixed styles

--- crates/sites/site_1/src/pages/layouts/app.rs
+++ crates/sites/site_1/src/pages/layouts/app.rs
@@ -2,10 +2,21 @@ use crate::prelude::*;
 
 pub fn app_layout(children: Markup) -> Markup {
 	let js = js! {
-		me("#menu").addEventListener("click", () => {
-			me("#layout").style.gridTemplateColumns = me("#layout").style.gridTemplateColumns === "500px 1fr" ? "var(--menu-width) 1fr" : "500px 1fr";
+		me("#menu").on("click", () => {
+			const expandedMenuSize = "500px 1fr";
+			const isExpanded = me("#layout").style.gridTemplateColumns === expandedMenuSize;
+			if (isExpanded) {
+				me("#layout").style.gridTemplateColumns = "var(--menu-width) 1fr";
+				me("img.logo").style.width = "100px";
+				me("img.logo").src = "/js/assets/tos_dark_small.svg";
+			} else {
+				me("#layout").style.gridTemplateColumns = expandedMenuSize;
+				me("img.logo").src = "/js/assets/tos_dark_large.svg";
+				me("img.logo").style.width = "400px";
+			}
 		});
 	};
+
 	html! {
 		div #layout {
 			div #menu {
@@ -49,28 +60,41 @@ fn styles() -> Markup {
 			}
 			#header {
 				grid-area: header;
-				background-color: #222;
+				background-color: #fff;
+				border-bottom: 1px solid #ddd;
+				box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 50px;
 			}
 			#content {
 				grid-area: content;
-				background-color: #444;
+				background-color: #fff;
 				margin: 0;
 				padding: 50px;
+				overflow: scroll;
 			}
 			#menu {
+				container-type: inline-size;
+
 				grid-area: menu;
 
-				background-color: #333;
+				background-color: #000;
 				display: flex;
 				flex-direction: column;
 				gap: 10px;
 				padding: var(--menu-padding-inline);
 				align-items: center;
+				box-shadow: rgba(0, 0, 0, 0.4) 2px 0px 4px, rgba(0, 0, 0, 0.3) 7px 0px 13px -3px;
+
+				@container (min-width: 120px) {
+					.menu-item {
+						width: 80%;
+					}
+				}
 			}
 			.menu-item {
 				width: var(--menu-item-width);
 				height: var(--menu-item-width);
 				background-color: #555;
+				transition: width 0.2s ease, height 0.2s ease;
 			}
 			.logo {
 				width: 100px;
