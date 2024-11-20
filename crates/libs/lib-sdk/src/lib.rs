3a727be867436d8365fb92b7f8dc05e5afdb44fa
Eran Boodnero <eboodnero@gmail.com>
2024-11-19 16:17:47 -0800

    fml

--- crates/libs/lib-sdk/src/lib.rs
+++ crates/libs/lib-sdk/src/lib.rs
@@ -1,13 +1,8 @@
-mod base_service;
 mod client;
 mod config;
 mod error;
-mod http_client;
 mod macros;
 pub mod prelude;
 mod resource;
-mod resource_service;
-mod simple_client;
-mod token_client;
 
 pub use error::{Error, Result};
--- crates/libs/lib-sdk/src/prelude.rs
+++ crates/libs/lib-sdk/src/prelude.rs
@@ -1,6 +1,4 @@
-pub use crate::base_service::BaseService;
 pub use crate::client::*;
 pub use crate::error::*;
 pub use crate::impl_service;
 pub use crate::resource::*;
-pub use crate::resource_service::*;
