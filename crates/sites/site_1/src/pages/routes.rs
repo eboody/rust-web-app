66c28d7700a679ecf12dc26e06f1b9b5d7a10184
Eran Boodnero <eboodnero@gmail.com>
2024-11-24 07:05:44 -0800

    separated js and css routes into sep router

--- crates/sites/site_1/src/pages/routes.rs
+++ crates/sites/site_1/src/pages/routes.rs
@@ -10,11 +10,23 @@ use tower_http::services::ServeFile;
 const PUBLIC_DIR: &str = "crates/sites/site_1/src/web-folder";
 
 pub fn main_router(mm: ModelManager) -> Router {
-	let Assets { main_js, main_css } = get_js_and_css_files();
-	dbg!("main_css: {}", &main_css);
-	dbg!("main_js: {}", &main_js);
 	Router::new()
 		.route("/", get(get_slash))
+		.nest("/ebooks", ebooks::router(mm.clone()))
+		.merge(js_and_css_routes())
+		.fallback_service(ServeDir::new(PUBLIC_DIR))
+}
+
+async fn get_slash() -> Result<Markup> {
+	Ok(layouts::base(layouts::app(html! {
+		div hx-get="/ebooks/popup/ffa4e238-daaf-4e00-9f4f-3944d1ff329f" hx-trigger="load" hx-swap="outerHTML" {}
+		//(ebooks::get_menu().await?)
+	})))
+}
+
+fn js_and_css_routes() -> Router {
+	let Assets { main_js, main_css } = get_js_and_css_files();
+	Router::new()
 		.route_service(
 			"/css",
 			ServeFile::new(format!(
@@ -29,13 +41,4 @@ pub fn main_router(mm: ModelManager) -> Router {
 				main_js
 			)),
 		)
-		.nest("/ebooks", ebooks::router(mm.clone()))
-		.fallback_service(ServeDir::new(PUBLIC_DIR))
-}
-
-async fn get_slash() -> Result<Markup> {
-	Ok(layouts::base(layouts::app(html! {
-		div hx-get="/ebooks/popup/ffa4e238-daaf-4e00-9f4f-3944d1ff329f" hx-trigger="load" hx-swap="outerHTML" {}
-		//(ebooks::get_menu().await?)
-	})))
 }
